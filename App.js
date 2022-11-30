2ba8db25a456f6478219540781e861feeb97757d
Ruben Ramirez <rubenrv95@hotmail.com>
2022-11-29 22:27:50 -0300

    Vista login

--- App.js
+++ App.js
@@ -1,12 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, TextInput, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+     const [user, setUser] = useState('');
+     const [password, setPassword] = useState('');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={require("./assets/logo.png")} />
+
+      <StatusBar style="auto"/>
+
+      <View style={styles.VistaInput}>
+        <TextInput style={styles.TextInput} placeholder="Usuario" placeholderTextColor= "#707070"  onChangeText={(email) => setUser(user)}/>
+      </View>
+
+      <View style={styles.VistaInput}>
+        <TextInput style={styles.TextInput} placeholder="Contraseña" placeholderTextColor= "#707070"  secureTextEntry={true} onChangeText={(password) => setPassword(password)}/>
+      </View>
+
+
+      <View style={styles.VistaInput}>
+      <TouchableOpacity style={styles.btn}>
+          <Text style={styles.btnText}>Conectarse</Text>
+      </TouchableOpacity>
+      </View>
+
+      <View style={styles.VistaInput}>
+        <Text style={styles.firmText}>Desarrollado por Rubén Ramírez 2022</Text>
+      </View>
+     
+
     </View>
+
+
   );
 }
 
@@ -17,4 +44,42 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  image: {
+    marginBottom: 40
+  },
+
+  TextInput: {
+      height: 50,
+      width: 150,
+      color: 'black',
+      textAlign: 'center',
+      backgroundColor: '#c9c9c9',
+      borderRadius: 10,
+  },
+
+  VistaInput: {
+      marginTop: 15,
+      borderRadius: 10,
+      alignItems: 'center',
+  },
+
+  firmText: {
+    marginTop: 80,
+    color: '#999999',
+  },
+
+  btn: {
+      backgroundColor: 'black',
+      borderRadius: 6,
+      alignItems:"center",
+      justifyContent:"center",
+      marginTop: 30,
+      width: 130,
+      height: 40,
+  },
+
+  btnText: {
+      color: 'white',
+  }
 });
